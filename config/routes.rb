2dd69882adee9867c27ad34e33dd5f1d7da76e52
bwluera <52203468+bwluera@users.noreply.github.com>
2023-10-06 06:25:02 -0500

    Remove page pending full implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root 'pages#experience'
+  root 'pages#about'
+  get '/about', to: 'pages#about', as: :about
   get '/experience', to: 'pages#experience', as: :experience
-  get '/projects', to: 'pages#projects', as: :projects
+  # Removed pending full implementation. Github is sufficient for now.
+  # get '/projects', to: 'pages#projects', as: :projects
   get '/education', to: 'pages#education', as: :education
   get '/skills', to: 'pages#skills', as: :skills
   get '/contact', to: 'pages#contact', as: :contact
