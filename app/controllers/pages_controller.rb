3ed887e4687c79e2a07908a29ac794295bf565a6
bwluera <52203468+bwluera@users.noreply.github.com>
2023-10-06 06:23:38 -0500

    Remove unnecessary controller methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,2 @@
 class PagesController < ApplicationController
-  def home
-
-  end
-
-  def experience
-
-  end
-
-  def projects
-
-  end
-
-  def skills
-
-  end
-
-  def education
-
-  end
-
-  def contact
-
-  end
 end
